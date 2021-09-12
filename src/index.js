56960f714a6de9bd3f24b129f243cf16057ba928
Ilya <IlyaAbdullinWork@yandex.ru>
2021-09-12 17:02:15 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import Post from '@models/Post'
-import json from './assets/json'
-import carImage from './assets/logo'
-import * as $ from 'jquery'
-
-import './bable.js'
-import './test.ts'
-
 import './styles/styles.css'
-import './styles/test.less'
-import './styles/testing.scss'
 
 
-console.log(1231232131232)
-
-const post = new Post('Webpaack post title', carImage)
-
-$('pre').html(post.toString())
-
-// console.log(post.img)
+console.log(1)
